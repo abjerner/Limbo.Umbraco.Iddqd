443b6d874501d95c0f12b12cad614c6b48d70387
Anders Bjerner <abjerner@skybrud.dk>
2024-04-15 17:24:31 +0200

    The value of the "IddqdPackage.InformationalVersion" property shouldn't include the SHA1 commit hash

--- src/Limbo.Umbraco.Iddqd/IddqdPackage.cs
+++ src/Limbo.Umbraco.Iddqd/IddqdPackage.cs
@@ -27,7 +27,9 @@ public static class IddqdPackage {
     /// <summary>
     /// Gets the informational version of the package.
     /// </summary>
-    public static readonly string InformationalVersion = FileVersionInfo.GetVersionInfo(typeof(IddqdPackage).Assembly.Location).ProductVersion!;
+    public static readonly string InformationalVersion = FileVersionInfo
+        .GetVersionInfo(typeof(IddqdPackage).Assembly.Location).ProductVersion!
+        .Split('+')[0];
 
     /// <summary>
     /// Gets the semantic version of the package.
